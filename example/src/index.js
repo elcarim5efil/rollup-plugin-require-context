171d55826cc89088fec306cb16ad0ced887c9407
elcarim5efil <elcarim5efil@gmail.com>
2019-03-29 18:02:41 +0800

    chore: update example

--- example/src/index.js
+++ example/src/index.js
@@ -1,4 +1,5 @@
-const n = require('./n');
-require.context('./imports', true, /.js$/);
-
+// import  n from './n';
+// require.context('./imports', true, /.\/[^a]/);
+// require.context('./imports', true, /.js$/);
+importAll(require.context('./imports', true));
 console.log('index');
