8a9ed173d7d10d747072d6395f1cdeb34290e787
elcarim5efil <elcarim5efil@gmail.com>
2019-03-29 18:03:25 +0800

    test: add test

--- test/index.spec.js
+++ test/index.spec.js
@@ -25,10 +25,10 @@ describe('collect left data', () => {
     expect(output.code).toContain('imports.sub.c');
   });
 
-  it('resursive with exp /./[^a]/', async () => {
+  it('resursive with exp /.\/[^a]/', async () => {
     const output = await build(`
       import './normal/index.js';
-      require.context('./imports', true, /./[^a]/);
+      require.context('./imports', true, /.\\/[^a]/);
     `);
     expect(output.code).toContain('normal.index');
     expect(output.code).not.toContain('imports.a');
@@ -47,5 +47,31 @@ describe('collect left data', () => {
     expect(output.code).not.toContain('imports.b');
     expect(output.code).toContain('imports.sub.index');
     expect(output.code).toContain('imports.sub.c');
+  });
+
+  it('work with comment', async () => {
+    const output = await build(`
+      import './normal/index.js';
+      // require.context('./imports');
+    `);
+    expect(output.code).toContain('normal.index');
+    expect(output.code).not.toContain('imports.a');
+    expect(output.code).not.toContain('imports.b');
+    expect(output.code).not.toContain('imports.sub.index');
+    expect(output.code).not.toContain('imports.sub.c');
+  });
+
+  it('should work with other function', async () => {
+    const output = await build(`
+      import './normal/index.js';
+      function importAll(r) {r.keys()}
+      importAll(require.context('./imports'));
+    `);
+    expect(output.code).toContain('normal.index');
+    expect(output.code).toContain('require_context_module');
+    expect(output.code).toContain('imports.a');
+    expect(output.code).toContain('imports.b');
+    expect(output.code).not.toContain('imports.sub.index');
+    expect(output.code).not.toContain('imports.sub.c');
   });
 });
